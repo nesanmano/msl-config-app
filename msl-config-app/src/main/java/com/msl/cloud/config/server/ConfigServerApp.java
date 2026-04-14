9745bbf15766f1bc68f5b08db99a2c90d080e7aa
nesanmano <nesanmano@hotmail.com>
2026-04-14 17:58:22 -0400

    cloud config server-app

---
+++ msl-config-app/src/main/java/com/msl/cloud/config/server/ConfigServerApp.java
@@ -0,0 +1,15 @@
+package com.msl.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class, args);
+    }
+}
