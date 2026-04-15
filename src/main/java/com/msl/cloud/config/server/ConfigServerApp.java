f7fc55fc18b6df518dc59cc86f74c8c59e0165af
nesanmano <nesanmano@hotmail.com>
2026-04-15 19:15:20 -0400

    fixed bootstrap issue

--- src/main/java/com/msl/cloud/config/server/ConfigServerApp.java
+++ src/main/java/com/msl/cloud/config/server/ConfigServerApp.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
